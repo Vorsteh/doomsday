b6acaefb708612d7c64f1f4266a51464d9037c04
Vorsteh <whyzoid@gmail.com>
2026-04-10 03:47:38 +0200

    Maybe?

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {RouterProvider } from "react-router-dom"
 import { router } from "./app/router.tsx"
 import './index.css'
+import "katex/dist/katex.min.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
